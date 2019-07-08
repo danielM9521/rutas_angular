7fa8ef5d4392911f395668977954f80437f99510
danielM9521 <daniel_murillo.95@hotmail.com>
2019-07-07 23:52:19 -0600

    tutorial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { PageErrorComponent } from './page-error/page-error.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'noticias', component: NoticiasComponent },
+  { path: 'servicios', component: ServiciosComponent },
+  { path:'**', component: PageErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+  
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { HomeComponent } from './home/home.component';
+import { PageErrorComponent } from './page-error/page-error.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoticiasComponent,
+    ServiciosComponent,
+    HomeComponent,
+    PageErrorComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
